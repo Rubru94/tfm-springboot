bbb61e745dbd74ff7f4573d43aa79582a7eadda1
Rubru94 <rubenru94@hotmail.com>
2020-07-19 18:56:16 +0200

    feat "define base data model": create endpoint rest & web for customer company

--- src/main/java/tfm/springboot/controller/WebController.java
+++ src/main/java/tfm/springboot/controller/WebController.java
@@ -9,14 +9,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import tfm.springboot.model.Company;
 import tfm.springboot.model.Customer;
+import tfm.springboot.model.industries.Industries;
+import tfm.springboot.model.industries.Logistic;
+import tfm.springboot.model.industries.Services;
+import tfm.springboot.service.CompanyService;
 import tfm.springboot.service.CustomerService;
+import tfm.springboot.service.LogisticIndService;
+import tfm.springboot.service.ServicesIndService;
 
 @Controller
 public class WebController {
 
 	@Autowired
 	private CustomerService customerService;
+
+	@Autowired
+	private CompanyService companyService;
+	
+	@Autowired
+	private LogisticIndService logisticIndService;
+	
+	@Autowired
+	private ServicesIndService servicesIndService;
 
 	@GetMapping("/")
 	public String customers(Model model) {
@@ -48,4 +64,49 @@ public class WebController {
 		return "redirect:/customer/" + customer.getId();
 	}
 	
+	@GetMapping("/customer/{id}/company")
+	public String showCompany(@PathVariable long id, Model model, Company company) {
+		
+		Customer customer = this.customerService.getCustomer(id);
+		if (customer == null) {
+			model.addAttribute("errorMessage", "Not found client id " + id);
+			return "error";
+		}
+		
+		model.addAttribute("company", customer.getCompany());
+		return "company";
+	}
+
+	@PostMapping("/customer/{id}/company")
+	public String setCompany(HttpSession session, @PathVariable long id, Model model, Company company) {
+
+		Customer customer = this.customerService.getCustomer(id);
+		if (customer == null) {
+			model.addAttribute("errorMessage", "Not found client id " + id);
+			return "error";
+		}
+		
+		Company com = new Company(company.getName(), company.getCountry(), company.getIndustry());		
+		
+		if (com.getIndustry().equals(Industries.LOGISTIC.getName())) {
+			Logistic logistic = new Logistic();
+			com.setLogistic(logistic);
+			this.logisticIndService.addLogisticInd(logistic);
+		}
+
+		if (com.getIndustry().equals(Industries.SERVICES.getName())) {
+			Services services = new Services();
+			com.setService(services);
+			this.servicesIndService.addServiceInd(services);
+		}
+		
+		this.companyService.addCompany(com);
+		
+		customer.setCompany(com);
+		this.customerService.addCustomer(customer);
+
+		return "company";
+		//return "redirect:/customer/" + customer.getId();
+	}
+
 }
